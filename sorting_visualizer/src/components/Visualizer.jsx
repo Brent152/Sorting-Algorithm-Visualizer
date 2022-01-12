06399a398a890fd118631b950fba48cfa7f329da
Brent152 <brentjulius152@gmail.com>
2022-01-11 19:02:49 -0700

    Color Functionality
    Changed array composition to objects with value and color attributes. Added  saveFrame because copying array's of objects was being passed as reference so duplicating was complicated.

--- sorting_visualizer/src/components/Visualizer.jsx
+++ sorting_visualizer/src/components/Visualizer.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import './Visualizer.css';
 // I need to make the size of the array bars be based on the size of the screen
 const ARRAY_SIZE = 100;
-const ANIMATION_SPEED = 5;
-const MAX_ARRAY_ELEMENT = 400;
+const ANIMATION_SPEED = 50;
+const MAX_ARRAY_ELEMENT_NUMBER = 400;
+const UNSORTED_COLOR = "rgb(219, 25, 25)", SORTING_COLOR = "rgb(52, 24, 211)", SORTED_COLOR = "rgb(31, 212, 31)";
 
 class Visualizer extends React.Component {
 
@@ -17,71 +18,89 @@ class Visualizer extends React.Component {
         
     }
 
+    // Set default state
     componentDidMount() {
         this.setState({array: this.newArray()});
         this.setState({frames: []});
     }
 
+    // Called by Generate New Array Button and sets the state to a new array
     generateArrayHandle = () => {
         this.setState({array: this.newArray()});
         this.setState({frames: []});
     }
 
+    // Creates a new array and returns it
     newArray = () => {
         const arr = [];
         for (let i = 0; i < ARRAY_SIZE; i++) {
-            arr.push(getRandomInt(5, MAX_ARRAY_ELEMENT));
+            let bar = {
+                value: getRandomInt(5, MAX_ARRAY_ELEMENT_NUMBER),
+                color: UNSORTED_COLOR,
+            }
+            arr.push(bar);
         }
         console.log("made new array");
         return arr;
     }
 
+    // Selection sort the array from Selection Sort Button
     selectionSort = () => {
         let minIndex, temp;
-        const arr = this.newArray();
-        // tHIS doESNT MAke anY SENSE
-        console.log(arr);
-        console.log("startselect");
+        // Brand new array copied from this.state.array
+        const arr = [];
+        for (const element of this.state.array) {
+            arr.push(element);
+        }
+        console.log("Start Selection Sort");
 
-        const framesDupe = [];
+        // Selection sort
         for (let startIndex = 0; startIndex < ARRAY_SIZE; startIndex++) {
             minIndex = startIndex;
             for (let curIndex = startIndex; curIndex < ARRAY_SIZE; curIndex++) {
-                if (arr[curIndex] < arr[minIndex]) {
+                if (arr[curIndex].value <= arr[minIndex].value) {
                     minIndex = curIndex;
                 }
             }
-            temp = arr[minIndex];
-            arr[minIndex] = arr[startIndex];
-            arr[startIndex] = temp;
-            console.log(arr);
-            framesDupe[startIndex] = arr.slice();
-        }
-        // ADDING ALL SORTED ARRAYS TO FRAME
-        console.log("framedupe");
-        console.log(framesDupe);
-        
-        this.setState({frames: framesDupe});
+            arr[minIndex].color = SORTING_COLOR;
+            arr[startIndex].color = SORTING_COLOR;
+            this.saveFrame(arr);
+            // Swap values
+            temp = arr[minIndex].value;
+            arr[minIndex].value = arr[startIndex].value;
+            arr[startIndex].value = temp;
 
-        console.log(this.state.array);
+            arr[minIndex].color = UNSORTED_COLOR;
+            arr[startIndex].color = SORTED_COLOR;
+            this.saveFrame(arr);
+        }
+        // Animate the frames array
         this.animate();
     }
 
+    // Loops through frames array slowly - called by sorting functions
     animate = () => {
-        console.log(this.state.frames);
         let i = 0;
         this.myInterval = setInterval(() => {
             this.setState({array: this.state.frames[i]});
             i++;
-            if (i === ARRAY_SIZE) {
+            if (this.state.frames[i] === undefined) {
                 clearInterval(this.myInterval);
             }
         }, ANIMATION_SPEED);
     }
+    
+    // Saves given array to this.state.frames
+    saveFrame = (arr) => {
+        const newArr = [];
+        for (const element of arr) {
+          // Make a copy of the object and then push it to the array
+          newArr.push({...element})
+        }
+        this.state.frames.push(newArr);
+    }
 
     render() { 
-
-        const {array} = this.state;
 
         return (
             <React.Fragment>
@@ -99,8 +118,8 @@ class Visualizer extends React.Component {
 
 
                 <div className="array-container">
-                    {array.map((value, index) => (
-                        <div className="array-bar" key={index} style={{height: `${value}px`}}></div>
+                    {this.state.array.map((bar, index) => (
+                        <div className="array-bar" key={index} style={{height: `${bar.value}px`, backgroundColor: bar.color}}></div>
                     ))}
                 </div>
 
